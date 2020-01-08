a0c7e1b729a41e88e3ab788a750edaa9ef8253f6
Peter Gagliardi <ptrgags@gmail.com>
2020-01-07 20:56:42 -0500

    Start working on chords
    The released handler doesn't quite work, but it's a start.

--- hooks.js
+++ hooks.js
@@ -1,15 +1,51 @@
 export class Hooks {
     constructor() {
-        this.recognizer = new SequenceRecognizer();
+        this.seq_key_recognizer = new SequenceRecognizer();
+        this.seq_code_recognizer = new SequenceRecognizer();
+        this.chord_key_recognizer = new ChordRecognizer();
+        this.chord_code_recognizer = new ChordRecognizer();
         this.pressed_keys = new Set();
+        this.pressed_codes = new Set();
     }
 
-    add_reset_sequence(path) {
-        this.recognizer.add_reset_sequence(path);
+    add_reset_key_sequence(path) {
+        this.seq_key_recognizer.add_reset_sequence(path);
     }
 
-    add_sequence(path, callback) {
-        this.recognizer.add_sequence(path, callback);
+    add_reset_code_sequence(path) {
+        this.seq_code_recognizer.add_reset_sequence(path);
+    }
+
+    add_key_sequence(path, callback) {
+        this.seq_key_recognizer.add_sequence(path, callback);
+    }
+
+    add_code_sequence(path, callback) {
+        this.seq_code_recognizer.add_sequence(path, callback);
+    }
+
+    add_key_chord_pressed(chord, callback) {
+        this.chord_key_recognizer.add_chord_pressed(chord, callback);
+    }
+
+    add_code_chord_pressed(chord, callback) {
+        this.chord_code_recognizer.add_chord_pressed(chord, callback);
+    }
+
+    add_key_chord_held(chord, callback) {
+        this.chord_key_recognizer.add_chord_held(chord, callback);
+    }
+
+    add_code_chord_held(chord, callback) {
+        this.chord_code_recognizer.add_chord_held(chord, callback);
+    }
+
+    add_key_chord_released(chord, callback) {
+        this.chord_key_recognizer.add_chord_released(chord, callback);
+    }
+
+    add_code_chord_released(chord, callback) {
+        this.chord_code_recognizer.add_chord_released(chord, callback);
     }
 
     listen(element) {
@@ -19,30 +55,37 @@ export class Hooks {
         }
 
         elem.addEventListener('keydown', (e) => {
-            if (this.pressed_keys.has(e.code)) {
+            if (this.pressed_codes.has(e.code)) {
                 this.on_key_held(e);
             } else {
-                this.pressed_keys.add(e.code);
+                this.pressed_keys.add(e.key);
+                this.pressed_codes.add(e.code);
                 this.on_key_pressed(e);
             }
         });
 
         elem.addEventListener('keyup', (e) => {
             this.on_key_released(e);
-            this.pressed_keys.delete(e.code);
+            this.pressed_keys.delete(e.key);
+            this.pressed_codes.delete(e.code);
         });
     }
 
     on_key_pressed(e) {
-        this.recognizer.on_key_pressed(e.code);
+        this.seq_key_recognizer.on_key_pressed(e.key);
+        this.seq_code_recognizer.on_key_pressed(e.code);
+        this.chord_key_recognizer.on_key_pressed(this.pressed_keys);
+        this.chord_code_recognizer.on_key_pressed(this.pressed_codes);
     }
 
     on_key_held(e) {
-        // TODO: Chords coming soon :)
+        this.chord_key_recognizer.on_key_held(this.pressed_keys);
+        this.chord_code_recognizer.on_key_held(this.pressed_codes);
     }
 
     on_key_released(e) {
-        // TODO: Chords coming soon :)
+        this.chord_key_recognizer.on_key_released(this.pressed_keys);
+        this.chord_code_recognizer.on_key_released(this.pressed_codes);
     }
 }
 
@@ -130,5 +173,127 @@ class SequenceRecognizer {
         }
 
         return false;
+    }
+}
+
+class ChordEntry {
+    constructor(keys) { 
+        this.chord = new Set(keys);
+        this.pressed_callback = undefined;
+        this.held_callback = undefined;
+        this.released_callback = undefined;
+        this.active = false;
+    }
+}
+
+function sets_equal(set_a, set_b) {
+    return is_subset(set_a, set_b) && is_subset(set_b, set_a);
+}
+
+function is_subset(set_a, set_b) {
+    for (const value of set_a) {
+        if (!set_b.has(value)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+class ChordRecognizer {
+    constructor() {
+        this.entries = [];
+    }
+
+    find_entry(chord) {
+        const chord_set = new Set(chord);
+        for (const entry of this.entries) {
+            if (sets_equal(entry.chord, chord_set)) {
+                return entry;
+            }
+        }
+
+        return undefined;
+    }
+
+    add_entry(entry) {
+        this.entries.push(entry);
+    }
+
+    add_chord_pressed(chord, callback) {
+        let entry = this.find_entry(chord);
+        if (entry !== undefined) {
+            entry.pressed_callback = callback;
+        } else {
+            entry = new ChordEntry(chord);
+            entry.pressed_callback = callback;
+            this.add_entry(entry);
+        }
+    }
+
+    add_chord_held(chord, callback) {
+        let entry = this.find_entry(chord);
+        if (entry !== undefined) {
+            entry.held_callback = callback;
+        } else {
+            entry = new ChordEntry(chord);
+            entry.held_callback = callback;
+            this.add_entry(entry);
+        }
+    }
+
+    add_chord_released(chord, callback) {
+        let entry = this.find_entry(chord);
+        if (entry !== undefined) {
+            entry.released_callback = callback;
+        } else {
+            entry = new ChordEntry(chord);
+            entry.released_callback = callback;
+            this.add_entry(entry);
+        }
+    }
+
+    on_key_pressed(pressed_keys) {
+        for (const entry of this.entries) {
+            if (entry.active) {
+                continue;
+            }
+
+            if (is_subset(entry.chord, pressed_keys)) {
+                if (entry.pressed_callback !== undefined) {
+                    entry.pressed_callback();
+                }
+                entry.active = true;
+            }
+        }
+    }
+
+    on_key_held(pressed_keys) {
+        for (const entry of this.entries) {
+            if (!entry.active) {
+                continue;
+            }
+
+            if (is_subset(entry.chord, pressed_keys)) {
+                if (entry.held_callback !== undefined) {
+                    entry.held_callback();
+                }
+            }
+        }
+    }
+
+    on_key_released(pressed_keys) {
+        for (const entry of this.entries) {
+            if (!entry.active) {
+                continue;
+            }
+
+            if (!is_subset(entry.chord, pressed_keys)) {
+                if (entry.released_callback !== undefined) {
+                    entry.released_callback();
+                }
+                entry.active = false;
+            }
+        }
     }
 }
--- index.js
+++ index.js
@@ -8,22 +8,29 @@ function log(message) {
     p.innerHTML += message + '<br/>';
 }
 
-hooks.add_sequence(['KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY'], () => {
+hooks.add_code_sequence(['KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY'], () => {
     log('Open the secret door');
 });
 
-hooks.add_sequence(['KeyQ', 'KeyW', 'KeyE'], () => {
+hooks.add_code_sequence(['KeyQ', 'KeyW', 'KeyE'], () => {
     log('You\'ve reached a fork in the road');
 });
 
-hooks.add_sequence(['KeyQ', 'KeyW', 'KeyE', 'KeyF'], () => {
+hooks.add_code_sequence(['KeyQ', 'KeyW', 'KeyE', 'KeyF'], () => {
     log('You went the wrong way!');
 });
 
-hooks.add_sequence(['KeyY', 'KeyT', 'KeyR', 'KeyE', 'KeyW', 'KeyQ'], () => {
+hooks.add_code_sequence(['KeyY', 'KeyT', 'KeyR', 'KeyE', 'KeyW', 'KeyQ'], () => {
     log('Open the super secret door');
 });
 
-hooks.add_reset_sequence(['Escape']);
+hooks.add_key_chord_pressed(['q', 'w', 'e'], () => {
+    log('chord pressed.');
+});
+hooks.add_key_chord_released(['q', 'w', 'e'], () => {
+    log('chord released');
+});
+
+hooks.add_reset_code_sequence(['Escape']);
 
 hooks.listen();
